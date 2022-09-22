4a65c6bbf8dc5dcf08052e7a7c6c1a8e0b4fa300
Snir Yulzari <105823746+sniryulzari@users.noreply.github.com>
2022-09-22 11:11:03 +0300

    noting

--- routes/Listing.js
+++ routes/Listing.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const ListingController = require("../controllers/listingController");
-const {getUserByEmailModel} = require("../models/usersModel");
 
 
 router.get("/", ListingController.getListing);
